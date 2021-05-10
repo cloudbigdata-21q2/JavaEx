5d88a205065446eaf654d5c73fe5f032ad593393
남승균 <skyun.nam@gmail.com>
2021-05-10 09:21:59 +0900

    StringBuffer

--- JavaEx/src/com/javaex/api/stringclass/StringEx.java
+++ JavaEx/src/com/javaex/api/stringclass/StringEx.java
@@ -4,7 +4,39 @@ public class StringEx {
 
 	public static void main(String[] args) {
 //		stringBasic();
-		usefulMethods();
+//		usefulMethods();
+		stringBufferEx();
+	}
+	
+	//	문자열 객체 : 연결, 메서드 수행시 매번 새 String 반환
+	//	StringBuffer : 버퍼 기반으로 작동
+	private static void stringBufferEx() {
+		//	버퍼 생성
+		StringBuffer sb = new StringBuffer("This");
+		
+		//	문자열 추가: append
+		sb.append(" is pencil");
+		//	문자열 삽입: insert
+		sb.insert(8, "my");
+		//	문자열 치환: replace
+		sb.replace(8, 10, "your ");
+		
+		System.out.println(sb);
+		
+		//	버퍼 사이즈 변경
+		sb.setLength(10);	//	내부 버퍼의 크기를 10으로 줄인다
+		System.out.println(sb);
+		
+		//	StringBuffer의 메서드들은 연속해서 호출 -> 메서드 체이닝
+		
+		StringBuffer sb2 = new StringBuffer("This")
+				.append(" is pencil")
+				.insert(8, "my")
+				.replace(8, 10, "your ");
+		String s = sb2.toString();
+		
+		System.out.println("결과:" + s);
+				
 	}
 	
 	private static void stringBasic() {
